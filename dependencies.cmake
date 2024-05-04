86391d0d710c6b1051e8a71632db3b7610a6d1a1
Juan José Quiroz Omaña <juanjqogm@gmail.com>
2024-05-03 20:37:55 -0500

    Added first example

---
+++ dependencies.cmake
@@ -0,0 +1,15 @@
+if(APPLE) #APPLE
+    INCLUDE_DIRECTORIES(
+        /usr/local/include/
+        /usr/local/include/eigen3
+        # Most recent versions of brew install here
+        /opt/homebrew/include
+        /opt/homebrew/include/eigen3
+    )
+    ADD_COMPILE_OPTIONS(-Werror=return-type -Wall -Wextra -Wmissing-declarations -Wredundant-decls -Woverloaded-virtual)
+    # The library is installed here when using the regular cmake ., make, sudo make install
+    LINK_DIRECTORIES(
+        /usr/local/lib/
+        /opt/homebrew/lib/
+        )
+endif()
---
+++ examples/joint_position_control_no_dynamics/joint_position_control_no_dynamics/main.cpp
@@ -0,0 +1,69 @@
+#include <dqrobotics/DQ.h>
+#include <dqrobotics/interfaces/coppeliasim/DQ_CoppeliaSimInterface.h>
+#include <dqrobotics/interfaces/coppeliasim/robots/URXCoppeliaSimRobot.h>
+
+using namespace DQ_robotics;
+using namespace Eigen;
+
+VectorXd compute_control_signal(const MatrixXd J,
+                                const VectorXd& q,
+                                const double& damping,
+                                const double& gain,
+                                const VectorXd task_error);
+
+int main()
+{
+    auto vi = std::make_shared<DQ_CoppeliaSimInterface>();
+    vi->connect();
+    vi->start_simulation();
+
+    // Load the model only if it is not already on the scene.
+    vi->load_model_from_model_browser_if_missing("/robots/non-mobile/UR5.ttm", "/UR5", true);
+
+    // Load the reference frames only if they are not already on the scene.
+    vi->load_model_from_model_browser_if_missing("/other/reference frame.ttm", "/Current_pose");
+    vi->load_model_from_model_browser_if_missing("/other/reference frame.ttm", "/Desired_pose");
+
+
+    auto robot = URXCoppeliaSimRobot("/UR5", vi, URXCoppeliaSimRobot::MODEL::UR5);
+    auto robot_model = robot.kinematics();
+    robot.set_robot_as_visualization_tool();
+
+    VectorXd u = VectorXd::Zero(6);
+    VectorXd q = robot.get_configuration_space_positions();
+    double gain = 10;
+    double T = 0.001;
+    double damping = 0.01;
+
+    VectorXd qd = ((VectorXd(6) <<  0.5, 0, 1.5, 0, 0, 0).finished());
+
+    DQ xd = robot_model.fkm(qd);
+    vi->set_object_pose("/Desired_pose", xd);
+
+
+    for (int i=0; i<2000; i++)
+    {
+        DQ x = robot_model.fkm(robot.get_configuration_space_positions());
+        vi->set_object_pose("/Current_pose", x);
+        MatrixXd J = robot_model.pose_jacobian(q);
+        MatrixXd Jt = robot_model.translation_jacobian(J, x);
+        VectorXd task_error = (x.translation()-xd.translation()).vec4();
+
+        u = compute_control_signal(Jt, q, damping, gain, task_error);
+        q = q + T*u;
+        robot.set_control_inputs(q);
+        std::cout<<"error: "<<task_error.norm()<<std::endl;
+    }
+    vi->stop_simulation();
+}
+
+VectorXd compute_control_signal(const MatrixXd J,
+                                const VectorXd& q,
+                                const double& damping,
+                                const double& gain,
+                                const VectorXd task_error)
+{
+    VectorXd u = (J.transpose()*J + damping*damping*MatrixXd::Identity(q.size(), q.size())).inverse()*
+        J.transpose()*(-gain*task_error);
+    return u;
+}
