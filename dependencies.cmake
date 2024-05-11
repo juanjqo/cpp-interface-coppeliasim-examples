b53ac60bba0394a1db7988cc7ca5801f31a526bb
Juancho <juanjqogm@gmail.com>
2024-05-11 01:35:31 -0500

    Updated the cmake

--- dependencies.cmake
+++ dependencies.cmake
@@ -1,3 +1,9 @@
+if(UNIX AND NOT APPLE)
+    FIND_PACKAGE(Eigen3 REQUIRED)
+    INCLUDE_DIRECTORIES(${EIGEN3_INCLUDE_DIR})
+    ADD_COMPILE_OPTIONS(-Werror=return-type -Wall -Wextra -Wmissing-declarations -Wredundant-decls -Woverloaded-virtual)
+endif()
+
 if(APPLE) #APPLE
     INCLUDE_DIRECTORIES(
         /usr/local/include/
@@ -12,4 +18,31 @@ if(APPLE) #APPLE
         /usr/local/lib/
         /opt/homebrew/lib/
         )
+endif()
+
+
+
+if(WIN32)
+    include(C:/vcpkg/scripts/buildsystems/vcpkg.cmake)
+    set(CMAKE_TOOLCHAIN_FILE C:/vcpkg/scripts/buildsystems/vcpkg.cmake)
+    set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS ON)
+    ADD_DEFINITIONS(-D_USE_MATH_DEFINES)
+    FIND_PACKAGE(Eigen3 CONFIG REQUIRED)
+    INCLUDE_DIRECTORIES(${EIGEN3_INCLUDE_DIR})
+    find_package(cppzmq CONFIG REQUIRED)
+
+    set(DQROBOTICS_PATH "C:/Program Files (x86)/dqrobotics")
+    add_library(dqrobotics SHARED IMPORTED)
+    set_target_properties(dqrobotics PROPERTIES
+        IMPORTED_LOCATION ${DQROBOTICS_PATH}/bin/dqrobotics.dll
+        IMPORTED_IMPLIB   ${DQROBOTICS_PATH}/lib/dqrobotics.lib
+        INTERFACE_INCLUDE_DIRECTORIES ${DQROBOTICS_PATH}/include)
+
+    set(DQROBOTICS_COPPELIASIM_PATH "C:/Program Files (x86)/dqrobotics-interface-coppeliasim")
+    add_library(dqrobotics-interface-coppeliasim SHARED IMPORTED)
+    set_target_properties(dqrobotics-interface-coppeliasim PROPERTIES
+        IMPORTED_LOCATION ${DQROBOTICS_COPPELIASIM_PATH}/bin/dqrobotics-interface-coppeliasim.dll
+        IMPORTED_IMPLIB   ${DQROBOTICS_COPPELIASIM_PATH}/lib/dqrobotics-interface-coppeliasim.lib
+        INTERFACE_INCLUDE_DIRECTORIES ${DQROBOTICS_COPPELIASIM_PATH}/include)
+
 endif()
